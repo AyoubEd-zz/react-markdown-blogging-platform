4316233d97b57a37cf2acc1ce674f380f291927f
aeddakhly <ayoube.eddakhly@gmail.com>
2020-02-25 21:35:39 +0100

    fix wording

--- src/resumeInfo.js
+++ src/resumeInfo.js
@@ -6,15 +6,14 @@ export const data = {
       title: "Software Engineer Intern",
       logo: "carians",
       description:
-        "I'm working on Carians back-office. I'm reponsible for the new platform, this includes gathering a huge amount of medical data, managing the databases and implementing the server code and the user interface that will consume it."
+        "Built an expert recommendation system, APIs with Golang, crawlers in python and managed the ElasticSearch cluster. Managed the infrastructure which includes managing Kubernetes clusters and CI/CD pipelines"
     },
     {
       company: "WaystoCap",
       time: "July - August 2018",
       title: "Software Engineer Intern",
       logo: "waystocap",
-      description:
-        "I worked on WaystoCaps trading platform. I implemented and deployed on AWS a Serverless GraphQl API, and then developed and plugin for the platform that consumes it."
+      description: "Contributed to the trading platform, built Serverless microservices and deployed them to AWS Lambda. Improved communication between traders on the platform by 30% by implementing a plugin that consumes these microservices, and allows the traders to get notified in real-time of changes on current deals."
     },
     {
       company: "MosaicLab",
@@ -22,7 +21,7 @@ export const data = {
       title: "Software Engineer Intern",
       logo: "mosaiclab",
       description:
-        "Designed and developed a Geo-based app to locate traffic accidents in real time and show them in a display with the ability to apply multiple functions like defining a specific zone for search and finding paths."
+        "Designed and developed a Geo-based app to locate traffic accidents in real time and diplays the location with other details on a map, with the ability to apply multiple actions like defining a specific zone for search and finding paths."
     }
   ],
   projects: [
@@ -32,10 +31,10 @@ export const data = {
       url: "https://github.com/AyoubEd/Serverless-Typescript-GraphQl-Commenting-API",
       tag: ["Serverless", "GraphQl", "TypeScript"],
       content:
-        "Serverless is a new trend for the deployment of cloud applications. Recently it has gained much popularity mainly due to the shift of enterprise application architectures to containers and microservices..."
+        "Serverless is a new trend for the deployment of cloud applications. Recently it has gained a lot of popularity mainly due to the shift of enterprise application architectures to containers and microservices..."
     }
   ],
   images: [{ title: "codeit2017" }],
   aboutme:
-    "Hello ! I am Ayoub, a software engineer fascinated by large-scale systems.\nI am interested in the new theories and technologies that help to build them. My main focus is on distributed systems and new technological advances in cloud computing.\nI'm a competitive programming enthusiast because a good infrastructure should always be complemented with well designed algorithms.\nIn short, I like to program, share knowledge and build meaningful projects!"
+    "Hello, I'm Ayoub, a software engineer fascinated by large-scale systems. I love sharing knowledge and building meaningful projects, and this website is my little corner of the internet where I do that."
 };
